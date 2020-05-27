ca172951cf651d81f9f13dc2ede47410ad389ec4
perry912 <perry9122@gmail.com>
2020-05-27 23:09:01 +0900

    Initial commit

---
+++ app/src/main/java/com/j2d2/lotto/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.j2d2.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
