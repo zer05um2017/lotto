508c3a4d6e0b0992771bd9827fa5081750c02745
perry912 <perry9122@gmail.com>
2020-05-27 23:20:37 +0900

    Added 2 Activity

---
+++ app/src/main/java/com/j2d2/lotto/ConstellationActivity.kt
@@ -0,0 +1,12 @@
+package com.j2d2.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstellationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constellation)
+    }
+}
---
+++ app/src/main/java/com/j2d2/lotto/NameActivity.kt
@@ -0,0 +1,12 @@
+package com.j2d2.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name)
+    }
+}
